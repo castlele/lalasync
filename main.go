69971fa4989f92188fa21576e540c72352345da1
castlele <castlelecs@gmail.com>
2025-06-14 17:46:59 +0300

    [feat]: add hello world web app in the main.go

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+func songsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from songs!"))
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/songs", songsHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
