62223727c2cc3504721e9e39461c9c01b658b8b4
castlele <castlelecs@gmail.com>
2025-06-15 22:11:38 +0300

    [refactor]: add godotenv lib test in main.go

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/castlele/lalasync
 
 go 1.23.4
+
+require (
+	github.com/golang-jwt/jwt/v5 v5.2.2 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
--- main.go
+++ main.go
@@ -3,13 +3,21 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/joho/godotenv"
 )
 
 func songsHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from songs!"))
+	w.Write([]byte("Hello, World"))
 }
 
 func main() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	http.HandleFunc("/songs", songsHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
